86b7064a25d1e69cbb02d4815c9bab0b6d27841e
Andrei Martins <amartins13@outlook.com>
2016-07-22 12:10:42 -0300

    Solves problem 3.
    Fixes older version which was in a infinite loop, because of a missing division of n/p
    inside the while loop in largestPrime function.
    
    Also, decomposes code by creating isPrime and nextPrime auxiliary functions.

--- largestPrime.py
+++ largestPrime.py
@@ -1,26 +1,53 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-P = []    # List of candidates for the Largest Prime Award!
 # Returns the largest prime factor of n
 def largestPrime(n):
-    global P        # Initialize empty list of candidates!
-    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]   # First few primes.
-    while n > 1:            # Until reach one, we need further investigation!
-        for p in primes:
-            r = 1           # r > 0 so we enter the first iteration
-            while r > 0:    # Until find a prime which successfully divides n
-                r = n%p
-                if r > 0:
-                    break   # If p is not a factor, try next p
-                else:
-                    P.append(p)    # Hooray! We found a candidate!
-                    largestPrime(n/p)
+    factors = []
+    
+    i = 1
+    while n > 1:
+        p = nextPrime(i)
+        if n % p  == 0:
+            factors.append(p)
+            n = n / p
+        else:
+            i = p
 
-    # Phew! That was a quite exhausting hunting. I'm knackered...
-    # ... but the winner is...:
-    print(max(P))
+    print(max(factors))   # And the largest prime award goes to...
 
-# Test
+# Tells whether n is prime
+def isPrime(n):
+    for i in range(2, 100):    # Starts in 2 because divides every number.
+        if n%i == 0 and n != i:
+            return False
+    return True
+
+# Given n return the first prime n < p < 10000
+def nextPrime(n):
+    for i in range(n+1, 10000):
+        if isPrime(i) == True:
+            return i
+    return 0    # If couldn't find the next prime
+
+# isPrime Testing
+##print('Is 7 prime? ' + str(isPrime(7)))
+##print('Is 12 prime? ' + str(isPrime(12)))
+##print('Is 11 prime? ' + str(isPrime(11)))
+##print('Is 29 prime? ' + str(isPrime(29)))
+##print('Is 20 prime? ' + str(isPrime(20)))
+
+# nextPrime Testing
+##print('Next prime after 7: ' + str(nextPrime(7)))
+##print('Next prime after 12: ' + str(nextPrime(12)))
+##print('Next prime after 11: ' + str(nextPrime(11)))
+##print('Next prime after 29: ' + str(nextPrime(29)))
+##print('Next prime after 20: ' + str(nextPrime(20)))
+
+# largestPrimeTesting
+largestPrime(12)
+largestPrime(17)
+largestPrime(147)
+largestPrime(13195)
 largestPrime(600851475143)
     
